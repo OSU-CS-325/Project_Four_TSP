3bb304cb839bfbf26e971f30fb00b8896a18a97f
Kyle Guthrie <guthriek@oregonstate.edu>
2016-11-21 21:38:14 -0800

    Add LaTeX report structure and files

---
+++ report/make_pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# I'm currently using TeX Live to convert the .tex files to PDF
+# http://www.tug.org/texlive/
+
+# Because we're using bibtex, it requires a somewhat silly compile
+# order to ensure all references are built correctly
+# http://tex.stackexchange.com/questions/204291/bibtex-latex-compiling
+latex main
+biber main
+latex main
+pdflatex main
+mv main.pdf Project4_Group12_report.pdf
