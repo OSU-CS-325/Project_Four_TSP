f15cee89a806dbfe07663d9d5027e97aa6dc1ee5
Michael Stramel <stramelm@flip1.engr.oregonstate.edu>
2016-11-25 09:14:47 -0800

    Committing DFSofMST in-work because I'm about to overhaul it

--- Alg01_DFS_of_MST/DFSofMST.py
+++ Alg01_DFS_of_MST/DFSofMST.py
@@ -12,22 +12,95 @@ import sys
 
 # -----------------------------------------------------------------------------	
 def dist(cityOne, cityTwo):
- 	dx = cityOne['x'] - cityTwo['x']
+	dx = cityOne['x'] - cityTwo['x']
 	dy = cityOne['y'] - cityTwo['y']
 	dxSq = math.pow(dx, 2)
 	dySq = math.pow(dy, 2)
 	return int(round(math.sqrt(dxSq + dySq)))
 
 # -----------------------------------------------------------------------------	
+def popMin(prqu):
+	minU = prqu[0]
+	prqu.remove(prqu[0])
+	return minU
+
+# -----------------------------------------------------------------------------	
+def decreaseKey(prqu, cost):
+	for i in range(len(prqu)):
+		for j in range(len(prqu)):
+			if cost[prqu[i]] < cost[prqu[j]]:
+				prqu[i] = prqu[i] + prqu[j]
+				prqu[j] = prqu[i] - prqu[j]
+				prqu[i] = prqu[i] - prqu[j]
+
+# -----------------------------------------------------------------------------	
+def primsAlg(adjMatrix):
+	cost = [sys.maxsize for x in range(len(adjMatrix))]
+	prev = [None for x in range(len(adjMatrix))]
+	prqu = [x for x in range(len(adjMatrix))]
+
+	cost[0] = 0
+	decreaseKey(prqu, cost)
+
+	while len(prqu) > 0:
+		u = popMin(prqu)
+
+		for v in [v for v in range(len(adjMatrix)) if adjMatrix[u][v] > 0 and u != v]:
+			dist = adjMatrix[u][v]
+			if v in prqu and dist < cost[v]:
+				prev[v] = u
+				cost[v] = dist
+				decreaseKey(prqu, cost)
+
+	return prev
+
+# -----------------------------------------------------------------------------	
+def mstToAdjList(adjMatrix, mst):
+	adjList = {}
+	for i in range(0, len(adjMatrix)):
+		adjV = {}
+		for j in range(0, len(mst)):
+			if i == mst[j]:
+				adjV[j] = adjMatrix[i][j]
+		adjList[i] = adjV
+	return adjList
+
+# -----------------------------------------------------------------------------	
+def dfs(adjList):
+	vstd = []
+	stack = []
+	stack.append(0)
+	print (stack)
+
+	while(len(stack) > 0):
+		u = stack[0]
+		stack.remove(stack[0])
+		if not (u in vstd):
+			vstd.append(u)
+			auxStack = []
+			for v, dist in adjList[u].items():
+				if not (v in vstd):
+					auxStack.append(v)
+			while len(auxStack) > 0:
+				v = auxStack[0]
+				stack.append(v)
+				auxStack.remove(v)
+			print (stack)
+	#for key, value in adjList.items():
+	#	print (key, value)
+
+	return vstd
+
+# -----------------------------------------------------------------------------	
 def main():
 	# get input file name from command line
 	if (len(sys.argv) != 2):
-		print 'ERROR: Exactly one argument expected.  See usage instructions.\n'
+		print ('ERROR: Exactly one argument expected.  See usage instructions.\n')
 		quit()
 	else:
 		inFil = sys.argv[1]
-        if not(os.path.isfile(inFil)):
-			print 'ERROR: File \'' + str(inFil) + '\' not found.\n'
+		if not(os.path.isfile(inFil)):
+			print ('ERROR: File \'' + str(inFil) + '\' not found.\n')
 			quit()
 
 	# open input/output files
@@ -42,23 +115,46 @@ def main():
 		thisCity = {'id':int(eachCity[0]), 'x':int(eachCity[1]), 'y':int(eachCity[2])}
 		cities.append(thisCity)
 
+	# init adjacency matrix for graph (every city connected to every other city)
+	adjMatrix = [[0 for x in range(len(cities))] for y in range(len(cities))]
+	for i in range(0, len(cities)):
+		for j in range(i + 1, len(cities)):
+			ij = dist(cities[i], cities[j])
+			adjMatrix[i][j] = ij
+			adjMatrix[j][i] = ij
+		print (adjMatrix[i])
+	
+	# create minimum spanning tree (MST) using Prim's algorithm which is
+	# faster on dense graphs than Kruskal's algorithm - I consider my graph
+	# to be dense because every vertex (city) is connected to every other
+	mst = primsAlg(adjMatrix)
+	print (mst)
+
+	# convert mst into adjacencyList
+	adjList = mstToAdjList(adjMatrix, mst)
+	print (adjList)
+
+	# get DFS discovered order
+	disc = dfs(adjList)
+	print (disc)	
+
 	# calc the total distance from city 0 to 1 to 2 to n-1 to n to 0
-	totalDist = 0
-	iterCities = iter(cities)
-	prevCity = cities[0]
-	next(iterCities) # skip the very first city
-	for eachCity in iterCities:
+#	totalDist = 0
+#	iterCities = iter(cities)
+#	prevCity = cities[0]
+#	next(iterCities) # skip the very first city
+#	for eachCity in iterCities:
 		# get distance to eachCity from the prevCity
-		totalDist = totalDist + dist(eachCity, prevCity) 
-		prevCity = eachCity
+#		totalDist = totalDist + dist(eachCity, prevCity) 
+#		prevCity = eachCity
 	# get distance from last city back to first city
-	totalDist = totalDist + dist(prevCity, cities[0])
+#	totalDist = totalDist + dist(prevCity, cities[0])
 
 	# write output to file
-	outFil.write(str(totalDist) + '\n')
-	iterCities = iter(cities)
-	for eachCity in iterCities:
-		outFil.write(str(eachCity['id']) + '\n')
+#	outFil.write(str(totalDist) + '\n')
+#	iterCities = iter(cities)
+#	for eachCity in iterCities:
+#		outFil.write(str(eachCity['id']) + '\n')
 
 	inFil.close()
 	outFil.close()
--- Alg01_DFS_of_MST/runAlg01.sh
+++ Alg01_DFS_of_MST/runAlg01.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 clear
-ALG="braindeadTour"
+ALG="DFSofMST"
 
 INSTANCE="example"
-for NUM in {1..3}
+for NUM in {1..1}
 do
 	INFIL=tsp_${INSTANCE}_${NUM}.txt
 	if [ -f $INFIL ]
 		then
 		echo "running instance $INSTANCE #$NUM with the $ALG algorithm..."
-		/usr/bin/python ${ALG}.py $INFIL
-		echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
-		/usr/bin/python ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
+		START=$(date +%s.%N)
+		/usr/bin/python3 ${ALG}.py $INFIL
+		END=$(date +%s.%N)
+		DT=$(echo "$END - $START" | bc)
+		DD=$(echo "$DT/86400" | bc)
+		DT=$(echo "$DT-86400*$DD" | bc)
+		DH=$(echo "$DT/3600" | bc)
+		DT=$(echo "$DT-3600*$DH" | bc)
+		DM=$(echo "$DT/60" | bc)
+		DS=$(echo "$DT-60*$DM" | bc)
+		printf "Total runtime: %d:%02d:%02d:%02.4f\n" $DD $DH $DM $DS
+		#echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
+		#/usr/bin/python3 ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
 		echo "---------------------------------------------------------------"
 	fi
 done
@@ -24,9 +34,19 @@ do
 	if [ -f $INFIL ]
 		then
 		echo "running instance $INSTANCE #$NUM with the $ALG algorithm..."
-		/usr/bin/python ${ALG}.py $INFIL
+		START=$(date +%s.%N)
+		/usr/bin/python3 ${ALG}.py $INFIL
+		END=$(date +%s.%N)
+		DT=$(echo "$END - $START" | bc)
+		DD=$(echo "$DT/86400" | bc)
+		DT=$(echo "$DT-86400*$DD" | bc)
+		DH=$(echo "$DT/3600" | bc)
+		DT=$(echo "$DT-3600*$DH" | bc)
+		DM=$(echo "$DT/60" | bc)
+		DS=$(echo "$DT-60*$DM" | bc)
+		printf "Total runtime: %d:%02d:%02d:%02.4f\n" $DD $DH $DM $DS
 		echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
-		/usr/bin/python ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
+		/usr/bin/python3 ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
 		echo "---------------------------------------------------------------"
 	fi
 done
