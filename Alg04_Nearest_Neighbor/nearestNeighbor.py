ead7e5ba98248485f542837c551bb0f2ed1868ad
Michael Stramel <stramelm@flip3.engr.oregonstate.edu>
2016-11-26 00:26:47 -0800

    Refactored nearestNeighbor.py to make it signicantly faster

--- Alg04_Nearest_Neighbor/nearestNeighbor.py
+++ Alg04_Nearest_Neighbor/nearestNeighbor.py
@@ -19,26 +19,6 @@ def dist(cityOne, cityTwo):
 	return int(round(math.sqrt(dxSq + dySq)))
 
 # -----------------------------------------------------------------------------	
-def nearestNeighbor(adjMatrix, start):
-	vstd = []
-	vstd.append(start)
-#	print (vstd)
-	u = start
-
-	while len(vstd) < len(adjMatrix):
-		dist = sys.maxsize
-		next = -1
-		for v in range(len(adjMatrix)):
-			if adjMatrix[u][v] < dist and not v in vstd:
-				next = v
-				dist = adjMatrix[u][v]
-		vstd.append(next)
-#		print (vstd)
-		u = next
-		
-	return vstd 
-
-# -----------------------------------------------------------------------------	
 def main():
 	# get input file name from command line
 	if (len(sys.argv) != 2):
@@ -64,50 +44,52 @@ def main():
 #		print(thisCity)
 
 	# init adjacency matrix for graph (every city connected to every other city)
-	adjMatrix = [[0 for x in range(len(cities))] for y in range(len(cities))]
-	for i in range(0, len(cities)):
-		for j in range(i + 1, len(cities)):
-			ij = dist(cities[i], cities[j])
-			adjMatrix[i][j] = ij
-			adjMatrix[j][i] = ij
-#		print (adjMatrix[i])
+	adjMatrix = [[-1 for x in range(len(cities))] for y in range(len(cities))]
 	
-	# get tour order
-	minDist = sys.maxsize
-	minTour = []
-	for i in range(0, len(adjMatrix)):
-		tourAdjMatrix = [x[:] for x in adjMatrix]
-		tour = nearestNeighbor(tourAdjMatrix, i)
-#		print (tour)	
-
-	 	# calc the total distance from city 0 to 1 to 2 to n-1 to n to 0
-		totalDist = 0
-		iterCities = iter(tour)
-		prevCity = cities[tour[0]]
-		next(iterCities) # skip the very first city
-		for eachItem in iterCities:
-			eachCity = cities[eachItem]
-			# get distance to eachCity from the prevCity
-#			print (eachCity)
-#			print (prevCity)
-			addDist = dist(eachCity, prevCity)
-			totalDist = totalDist + addDist 
-#			print ("%s: %s" % (addDist, totalDist))
-			prevCity = eachCity
-		# get distance from last city back to first city
-		addDist = dist(prevCity, cities[tour[0]])
-		totalDist = totalDist + addDist 
-#		print ("%s: %s" % (addDist, totalDist))
-#		print (totalDist)
-		if totalDist < minDist:
-			minTour = []
-			minDist = totalDist
-			for j in range(0, len(adjMatrix)):
-				minTour.append(tour[j])
+	# look for tour starting at each possible vertex
+	minTourDist = sys.maxsize
+	minTourOrder = []
+	for i in range(0, len(cities)):
+		print("start at: " + str(i))
+		tourCities = [x for x in cities]
+		tourOrder = []
+		tourOrder.append(tourCities[i]['id'])
+#		print("tourOrder: " + str(tourOrder))
+		tourCities.remove(tourCities[i])
+		tourDist = 0
+		while len(tourCities) > 0:
+			# find closest city
+			thisCity = cities[tourOrder[len(tourOrder) - 1]]
+#			print("thisCity: " + str(thisCity))
+			minDist = sys.maxsize
+			minCity = -1
+#			print("tourCities: " + str(tourCities))
+			for j in range(0, len(tourCities)):
+				thisDist = adjMatrix[thisCity['id']][tourCities[j]['id']]
+#				print("thisDist: " + str(thisDist))
+				if thisDist == -1:
+					thisDist = dist(thisCity, tourCities[j])
+#					print("thisDist: " + str(thisDist))
+					adjMatrix[thisCity['id']][tourCities[j]['id']] = thisDist
+					adjMatrix[tourCities[j]['id']][thisCity['id']] = thisDist
+				if thisDist < minDist:
+					minDist = thisDist
+					minCity = tourCities[j]
+			tourOrder.append(minCity['id'])
+#			print("minDist: " + str(minDist))
+#			print("tourOrder: " + str(tourOrder))
+			tourCities.remove(minCity)
+			tourDist = tourDist + minDist
+		tourDist = tourDist + dist(cities[tourOrder[0]], cities[tourOrder[len(tourOrder) - 1]])
+#		print("tourOrder: " + str(tourOrder))
+#		print("thisDist:" + str(tourDist))
+		if tourDist < minTourDist:
+			minTourDist = tourDist
+			minTourOrder = [x for x in tourOrder]
 
 	# write output to file
-	outFil.write(str(minDist) + '\n')
-	iterCities = iter(minTour)
+	outFil.write(str(minTourDist) + '\n')
+	iterCities = iter(minTourOrder)
 	for eachCity in iterCities:
 		outFil.write(str(eachCity) + '\n')
 
