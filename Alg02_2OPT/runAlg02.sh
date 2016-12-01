db012a7554d6742a9e2eac9bcc77d8267c6cd1ff
Kyle Guthrie <guthriek@oregonstate.edu>
2016-12-01 13:09:16 -0800

    Tweak run script for re-running Alg02

--- Alg02_2OPT/runAlg02.sh
+++ Alg02_2OPT/runAlg02.sh
@@ -35,30 +35,6 @@ do
 		then
 		echo "running instance $INSTANCE #$NUM with the $ALG algorithm..."
 		START=$(date +%s.%N)
-		./${ALG} $INFIL
-		END=$(date +%s.%N)
-		DT=$(echo "$END - $START" | bc)
-		DD=$(echo "$DT/86400" | bc)
-		DT=$(echo "$DT-86400*$DD" | bc)
-		DH=$(echo "$DT/3600" | bc)
-		DT=$(echo "$DT-3600*$DH" | bc)
-		DM=$(echo "$DT/60" | bc)
-		DS=$(echo "$DT-60*$DM" | bc)
-		printf "Total runtime: %d:%02d:%02d:%02.4f\n" $DD $DH $DM $DS
-		echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
-		/usr/bin/python3 ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
-		echo "---------------------------------------------------------------"
-	fi
-done
-
-INSTANCE="test-input-"
-for NUM in {0..0}
-do
-	INFIL=${INSTANCE}${NUM}.txt
-	if [ -f $INFIL ]
-		then
-		echo "running instance $INSTANCE #$NUM with the $ALG algorithm..."
-		START=$(date +%s.%N)
 		../TSP_Files/watch.py ./${ALG} $INFIL
 		END=$(date +%s.%N)
 		DT=$(echo "$END - $START" | bc)
@@ -72,5 +48,32 @@ do
 		echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
 		/usr/bin/python3 ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
 		echo "---------------------------------------------------------------"
+		mv ${INFIL}.tour ${INFIL}.3min.tour
 	fi
 done
+
+INSTANCE="test-input-"
+for NUM in {1..6}
+do
+	INFIL=${INSTANCE}${NUM}.txt
+	if [ -f $INFIL ]
+		then
+		echo "running instance $INSTANCE #$NUM with the $ALG algorithm..."
+		START=$(date +%s.%N)
+		./${ALG} $INFIL
+		END=$(date +%s.%N)
+		DT=$(echo "$END - $START" | bc)
+		DD=$(echo "$DT/86400" | bc)
+		DT=$(echo "$DT-86400*$DD" | bc)
+		DH=$(echo "$DT/3600" | bc)
+		DT=$(echo "$DT-3600*$DH" | bc)
+		DM=$(echo "$DT/60" | bc)
+		DS=$(echo "$DT-60*$DM" | bc)
+		printf "Total runtime: %d:%02d:%02d:%02.4f\n" $DD $DH $DM $DS
+		echo "verifying results of $INSTANCE #$NUM with the $ALG algorithm..."
+		/usr/bin/python3 ../TSP_Files/tsp-verifier.py $INFIL ${INFIL}.tour
+		echo "---------------------------------------------------------------"
+		mv ${INFIL}.tour ${INFIL}.unlim.tour
+	fi
+done
+
